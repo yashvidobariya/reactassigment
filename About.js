053261909ce670e2274c4eac25e3ec523764cf23
yashvidobariya <109908282+yashvidobariya@users.noreply.github.com>
2022-08-15 10:16:40 +0530

    Add files via upload

--- About.js
+++ About.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from './Header'
 
-export default function Contact() {
+export default function About() {
   return (
-    <div>contactus</div>
+    <div>About</div>
   )
 }
--- Header.js
+++ Header.js
@@ -2,47 +2,70 @@ import React from 'react'
 import custom from "./custom.module.css";
 import { LinkContainer } from "react-router-bootstrap";
 
-
 export default function Header() {
+    
     return (
         <>
-        <nav class={`navbar navbar-expand-lg ${custom.bglight}`}>
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <LinkContainer to="/Home">
-                        <li class="nav-item">
-                        <a className={`nav-link active ${custom.navLink}`}
+            <nav className={`navbar navbar-expand-lg ${custom.bglight}`}>
+                <div className="container-fluid">
+                    <div className={`logo ${custom.logo}`}><span>𝖕𝖊𝖗𝖋𝖚𝖒𝖊</span></div>&nbsp;&nbsp;
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <LinkContainer to="/Home1">
+                                <li className="nav-item">
+                                    <a className={`nav-link active ${custom.navLink}`}
                                         aria-current="page"
                                         href="#">Home</a>
-                        </li>
-                        </LinkContainer>
-                        <LinkContainer to="/Aboutus">
-                        <li class="nav-item">
-                        <a className={`nav-link active ${custom.navLink}`}
+                                </li>
+                            </LinkContainer>
+
+                            <LinkContainer to="/About">
+                                <li className="nav-item">
+                                    <a className={`nav-link active ${custom.navLink}`}
                                         aria-current="page"
                                         href="#">About us</a>
-                        </li>
-                        </LinkContainer>
-                        <LinkContainer to="/Contact">
-                        <li class="nav-item">
-                        <a className={`nav-link active ${custom.navLink}`}
+                                </li>
+                            </LinkContainer>
+
+                            <LinkContainer to="/products">
+                                <li className="nav-item">
+                                    <a className={`nav-link active ${custom.navLink}`}
                                         aria-current="page"
-                                        href="#">contactus</a>
-                        </li>
-                        </LinkContainer>
+                                        href="#">Products</a>
+                                </li>
+                            </LinkContainer >
+
+                            <LinkContainer to="/Contact">
+                                <li class="nav-item">
+                                    <a className={`nav-link active ${custom.navLink}`}
+                                        aria-current="page"
+                                        href="#">contact us</a>
+                                </li>
+                            </LinkContainer>
+
+
                         </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
+            </nav>
+            {/* <i class="fa-solid fa-cart-shopping"></i>
+            <div className="container">
+            <div className="row mt-5">
+                <Products/>
+                <Products/>
+                <Products/>
+                <Products/>
+                <Products/>
+                <Products/>
             </div>
-        </nav>
+            </div> */}
         </>
     )
 }
